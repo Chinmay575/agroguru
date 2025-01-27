600771611af22e0f140a8bd6ddf456bacbe41883
Chinmay575 <chinmaysinghmodak575@gmail.com>
2025-01-27 08:07:45 +0530

    Merge

--- lib/src/utils/constants/strings/urls.dart
+++ lib/src/utils/constants/strings/urls.dart
@@ -1,11 +1,11 @@
 class Urls {
   static String local = '127.0.0.1:8000';
 
-  static String emulator = '10.0.2.2:8000';
+  static String emulator = '10.0.2.2';
 
   static String aws = '18.177.13.5';
 
-  static String localApiUrl = 'http://$aws:8000/api/v1/';
+  static String localApiUrl = 'http://$emulator:8000/api/v1/';
 }
 
 class Endpoints {
